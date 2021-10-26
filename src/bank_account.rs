118caba1107c5c158e45c86699776341572d881a
unknown <sarah.nguyen@utt.fr>
2021-10-26 21:40:11 +0200

    Create withdraw method

--- src/bank_account.rs
+++ src/bank_account.rs
@@ -28,6 +28,21 @@ impl BankAccount {
             Ok(amount)
         }
     }
+
+    fn withdraw_amount(&mut self, amount: f32) -> Result<f32,&str> {
+        if amount <= 0.0 {
+            Err("Withdraw : cannot withdraw an amount below 0")
+        } else {
+            let transaction = Transaction {
+                date: chrono::offset::Local::now(),
+                amount: amount * (-1.0)
+            };
+
+            self.transactions.push(transaction);
+            Ok(amount)
+        }
+    }
+
 }
 
 #[test]
@@ -39,6 +54,19 @@ fn deposit_positive_amount() {
 #[test]
 #[should_panic]
 fn deposit_negative_amount() {
+    let mut bank_account = BankAccount::new(1, &Vec::new());
+    assert_eq!(bank_account.deposit_amount(-1.0), Ok(-1.0));
+}
+
+#[test]
+fn withdraw_positive_amount() {
+    let mut bank_account = BankAccount::new(1, &Vec::new());
+    assert_eq!(bank_account.deposit_amount(1.0), Ok(1.0));
+}
+
+#[test]
+#[should_panic]
+fn withdraw_negative_amount() {
     let mut bank_account = BankAccount::new(1, &Vec::new());
     assert_eq!(bank_account.deposit_amount(-1.0), Ok(-1.0));
 }
